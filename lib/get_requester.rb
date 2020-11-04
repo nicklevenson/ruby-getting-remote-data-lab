d98cef0ac722c9f571598a9ba164260d6e64811f
Nicholas Levenson <nicklevenson@gmail.com>
2020-11-04 13:08:27 -0800

    commit tree d1a6951db491dc4af6203da52766f182826f4a2a

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
 # Write your code here
+require 'json'
+require 'open-uri'
+require 'net/http'
+class GetRequester
+    attr_accessor :url
+    def initialize(string)
+        @url = string
+        get_response_body
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        JSON.parse(get_response_body)
+    end
+
+
+
+
+
+end
